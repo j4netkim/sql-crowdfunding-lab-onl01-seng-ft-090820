708e7edb70470fb24475a31dece832ac01ee8847
J K <jkim65@buffalo.edu>
2020-10-08 22:26:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,8 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   "SELECT users.name, users.age, SUM(pledges.amount)
   FROM pledges
   LEFT OUTER JOIN users
-  ON pledges.user_id = users.id
-
+  ON pledges.user_id = users.id GROUP BY users.name ORDER BY users.name ASC
   "
 end
 
